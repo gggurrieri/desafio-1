46e73849e8567bc9c2b731ee36d3d4c0b74dc18d
gabrielgurrieri <gggurrieri@gmail.com>
2022-07-31 00:35:37 -0300

    ejercicio con FOR

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
-
+/*
 let diasDeLaSemana = prompt("Ingresa los dias de la semana e ingresa domingo para finalizar")
 while(diasDeLaSemana != "domingo"){
     diasDeLaSemana = prompt("Ingresa los dias de la semana e ingresa domingo para finalizar")
     console.log("Ingresaste el dia " + diasDeLaSemana)
+}
+*/
+
+for(let i = 1; i <= 5; i++){
+let diasDeLaSemana = prompt("Ingresa los dias de la semana")
+alert(i + " Ingresaste el dia " + diasDeLaSemana)
 }
