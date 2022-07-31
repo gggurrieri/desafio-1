a3bea2bb450ef2e4e58c805d469f84b10e1e850d
gabrielgurrieri <gggurrieri@gmail.com>
2022-07-31 00:14:37 -0300

    dias de la semana

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+let diasDeLaSemana = prompt("Ingresa los dias de la semana e ingresa domingo para finalizar")
+while(diasDeLaSemana != "domingo"){
+    diasDeLaSemana = prompt("Ingresa los dias de la semana e ingresa domingo para finalizar")
+    console.log("Ingresaste el dia " + diasDeLaSemana)
+}
